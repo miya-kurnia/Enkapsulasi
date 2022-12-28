c2eb33e371b0bb5731c7750ce203f53c99e0e947
miya-kurnia <115356520+miya-kurnia@users.noreply.github.com>
2022-12-28 13:26:53 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,29 @@
+package Enkapsulasi;
+
+public class Person {
+    private final String name;
+    private String address;
+   
+   public Person(String name, String address) {
+      this.name = name;
+      this.address = address;
+   }
+   
+   public String getName() {
+      return name;
+   }
+  
+   
+   public String getAddress() {
+      return address;
+   }
+   
+   public void setAddress(String address) {
+      this.address = address;
+   }
+   
+   @Override
+   public String toString() {
+      return name + "(" + address + ")";
+   }
+}
